d806d38fd663e896f2fa852cf9eed995b010758e
Mykola Skorenkyy <mykola.skorenky@gmail.com>
2013-01-03 18:25:29 +0200

    test index file

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Mykola_Skorenkyi
+ * Date: 1/3/13
+ * Time: 6:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+phpinfo();
